04123250a86455731f9f706701117bef3156b604
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-11-03 13:39:16 +0000

    feat: added spend Item object structure and to spend_item add function

---
+++ src/data/item_structure.py
@@ -0,0 +1,2 @@
+class SpendItemStructure:
+  amount: int
--- src/data/spend_item.py
+++ src/data/spend_item.py
@@ -1,18 +1,21 @@
 from typing import List
 
-from models.spend_item import SpendItem
 from data.db_setup import get_tenant_db
+from data.item_structure import SpendItemStructure
+
+from models.spend_item import SpendItem
 
 def get_spend_items() -> List[SpendItem]:
-  db_session = get_tenant_db()
-  return db_session.query(SpendItem).all()
+  session, _ = get_tenant_db()
+  return session.query(SpendItem).all()
 
 # TODO: Add spend type properly 
-def add_spend_item(spend_item: SpendItem):
-  db_session = get_tenant_db()
-  db_session.add(spend_item)
-  db_session.commit()
+def add_spend_item(spend_item: SpendItemStructure):
+  spend_item_object = SpendItem(amount=spend_item.amount)
+  session, _ = get_tenant_db()
+  session.add(spend_item_object)
+  session.commit()
 
-def delete_spend_item(item_id: int):
-  db_session = get_tenant_db()
-  db_session.query
+# def delete_spend_item(item_id: int):
+#   db_session = get_tenant_db()
+#   db_session.query
